28dc21070ca67e119f5c8a5fddeb3814b08bbf14
d.darroudi <danialdarroudy1383@gmail.com>
2024-08-12 14:35:32 +0330

    init

--- ETL/ETLProject/obj/Debug/net8.0/ETLProject.AssemblyInfo.cs
+++ ETL/ETLProject/obj/Debug/net8.0/ETLProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETLProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+371ffbba14597d60cb5f9c49ef48ac14cd329eeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETLProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETLProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
